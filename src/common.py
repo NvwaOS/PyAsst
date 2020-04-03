a02c783a71600367cf92bdcb3ed6975d980e838a
Singu <singu@singu.top>
2020-04-04 06:52:34 +0800

    增加文件系统工具类，并对字符串工具类增加类型约束

--- src/common.py
+++ src/common.py
@@ -1,8 +1,9 @@
+import os
 
 
 class StringUtil:
     @classmethod
-    def decode(cls, content, encodes, encoding):
+    def decode(cls, content: bytes, encodes: str, encoding: str) -> str:
         for code in encodes:
             if code is None:
                 continue
@@ -14,3 +15,18 @@ class StringUtil:
             return content.decode(encoding, errors='ignore')
         raise RuntimeError('无法确定内容的编码格式，已尝试的编码格式有: %s' % list(filter(bool, encodes)))
 
+
+class FileSystemUtil:
+    @classmethod
+    def split(cls, filepath: str) -> (str, str, str):
+        dirpath, filename = os.path.split(filepath)
+        name, ext = os.path.splitext(filename)
+        return dirpath, name, ext
+
+    @classmethod
+    def make_if_doesnt_exist(cls, path: str) -> None:
+        if not path:
+            return
+        path = os.path.abspath(path)
+        if not os.path.exists(path):
+            os.makedirs(path)
