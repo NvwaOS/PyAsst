ee2b48e831e3688feb4bba4dd61ba5d420084bb2
Singu <singu@singu.top>
2020-08-07 20:21:35 +0800

    以新的规范，重新编写 cv2 模块

--- pyasst/ext/cv2.py
+++ pyasst/ext/cv2.py
@@ -1,28 +1,44 @@
 import os
-from typing import Any, Union
-from ..common import FileSystemUtil
+from pathlib import Path
+from typing import Any, Union, IO
+from ..common import ModuleIsNotInstallError, FileSystemUtil
 
-
-def __none_function__(*argv, **kwargs) -> None:
-    raise Exception("相关第三方包未安装，所以该函数未实现。")
-
-
-# 增加 OpenCV 功能拓展
-cv_imread = __none_function__
-cv_imwrite = __none_function__
 try:
     import cv2 as cv
+except ModuleNotFoundError as E:
+    raise ModuleIsNotInstallError('OpenCV', 'pip install opencv-python') from E
+
+try:
     import numpy as np
+except ModuleNotFoundError as E:
+    raise ModuleIsNotInstallError('Numpy', 'pip install numpy') from E
 
-    def cv_imread(filename: Any, flags: Any = cv.IMREAD_ANYCOLOR) -> Union[None, np.ndarray]:
-        return cv.imdecode(np.fromfile(filename, dtype=np.uint8), flags)
 
-    def cv_imwrite(filename: Any, img: Any, params: Any = None):
-        dirpath, name, ext = FileSystemUtil.split(filename)
-        FileSystemUtil.make_if_doesnt_exist(dirpath)
-        if not ext:
-            ext = '.png'
-        filepath = os.path.join(dirpath, name + ext)
-        cv.imencode(ext, img, params)[1].tofile(filepath)
-except ModuleNotFoundError:
-    pass
+def imread(file: Union[IO, str, Path], flags: int = cv.IMREAD_ANYCOLOR) -> Union[None, np.ndarray]:
+    """
+    Support imread of Chinese path.
+    支持中文路径的 imread。
+
+    :param file: file or str or Path
+    :param flags: The same flags as in cv::imread, see cv::ImreadModes.
+    :return: Image
+    """
+    return cv.imdecode(np.fromfile(file, dtype=np.uint8), flags)
+
+
+def imwrite(file: Union[IO, str, Path], img: np.ndarray, params: Any = None):
+    """
+    Support imwrite of Chinese path.
+    支持中文路径的 imwrite。
+
+    :param file: file or str or Path
+    :param img: Image to be written.
+    :param params: Format-specific parameters. See cv::imwrite and cv::ImwriteFlags.
+    :return:
+    """
+    dirpath, name, ext = FileSystemUtil.split(file)
+    FileSystemUtil.make_if_doesnt_exist(dirpath)
+    if not ext:
+        ext = '.png'
+    filepath = os.path.join(dirpath, name + ext)
+    cv.imencode(ext, img, params)[1].tofile(filepath)
